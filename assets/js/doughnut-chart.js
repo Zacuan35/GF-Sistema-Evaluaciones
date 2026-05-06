ba61fd138a8bb0ff7658de5d89629470f1fc1ad4
Zacuan <dg.luism19@gmai.com>
2026-05-06 13:43:44 -0600

    Reducción de tamaño de gráficas

--- assets/js/doughnut-chart.js
+++ assets/js/doughnut-chart.js
@@ -2,11 +2,23 @@ const objectivesChart = document.getElementById('objectivesChart');
 
 new Chart(objectivesChart, {
   type: 'doughnut',
+
   data: {
     labels: ['Completado', 'Pendiente'],
+
     datasets: [{
       data: [80, 20]
     }]
+  },
+
+  options: {
+    responsive: false,
+
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
   }
 });
 
@@ -14,10 +26,22 @@ const skillsChart = document.getElementById('skillsChart');
 
 new Chart(skillsChart, {
   type: 'doughnut',
+
   data: {
     labels: ['Aprobado', 'Faltante'],
+
     datasets: [{
       data: [60, 40]
     }]
+  },
+
+  options: {
+    responsive: false,
+
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
   }
 });
