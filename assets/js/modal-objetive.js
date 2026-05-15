93990beb0c79f460b10dc97eb9bc2da46489f510
Zacuan <dg.luism19@gmai.com>
2026-05-15 17:43:36 -0600

    Ajuste de script de moda-02

--- assets/js/modal-objetive.js
+++ assets/js/modal-objetive.js
@@ -10,24 +10,24 @@ document.querySelectorAll('[data-modal]').forEach(button => {
 
 document.querySelectorAll('[data-dismiss="modal"]').forEach(button => {
   button.addEventListener('click', function() {
-      const modal = this.closest('.modal');
+      const modal = this.closest('.modal-objetive');
       closeModal(modal.id);
   });
 });
 
 function openModal(modalId) {
   const modal = document.getElementById(modalId);
-  modal.style.display = "flex"; // Usamos "flex" para mostrar el modal centrado
+  modal.style.display = "flex";
 }
 
 function closeModal(modalId) {
   const modal = document.getElementById(modalId);
-  modal.style.display = "none"; // Usamos "none" para ocultar el modal
+  modal.style.display = "none";
 }
 
 // Cerrar el modal al hacer clic fuera de él
 window.onclick = function(event) {
-  if (event.target.classList.contains('modal')) {
+  if (event.target.classList.contains('modal-objetive')) {
       closeModal(event.target.id);
   }
 };
