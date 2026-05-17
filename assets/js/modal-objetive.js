663f7401102a29a81d335a5de8766af165a0b0b7
Zacuan <dg.luism19@gmai.com>
2026-05-16 21:33:07 -0600

    Se agrega el btn-toggle-panel

--- assets/js/modal-objetive.js
+++ assets/js/modal-objetive.js
@@ -31,3 +31,31 @@ window.onclick = function(event) {
       closeModal(event.target.id);
   }
 };
+
+/* =====================
+# TOGGLE PANEL
+===================== */
+document.querySelectorAll('.btn-toggle-panel').forEach(button => {
+
+  button.addEventListener('click', function() {
+
+    // Modal actual
+    const modal = this.closest('.modal-objetive');
+
+    // Toggle clase
+    modal.classList.toggle('has-panel');
+
+    // Ícono
+    const icon = this.querySelector('i');
+
+    if (modal.classList.contains('has-panel')) {
+      icon.classList.remove('icon-eye');
+      icon.classList.add('icon-eye-slash');
+    } else {
+      icon.classList.remove('icon-eye-slash');
+      icon.classList.add('icon-eye');
+    }
+
+  });
+
+});
