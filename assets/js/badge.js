3809a923ab314cd9583a747cd9e2251a3dcc7ee1
Zacuan <dg.luism19@gmai.com>
2026-06-15 14:37:46 -0600

    Ajuste de modal-colaborados

--- assets/js/badge.js
+++ assets/js/badge.js
@@ -3,7 +3,18 @@
 ===================== */
 document.querySelectorAll(".badge-close")
 .forEach(btn => {
+
   btn.addEventListener("click", () => {
-    btn.closest(".badge").classList.add("is-hidden");
+
+    const badge = btn.closest(".badge");
+
+    badge.classList.add("is-hidden");
+
+
+    setTimeout(() => {
+      badge.remove();
+    }, 300);
+
   });
+
 });
